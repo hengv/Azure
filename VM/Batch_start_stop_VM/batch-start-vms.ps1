55497da61b4c56de4909b251debe198b89db59b8
Heng Wei <hengwei@microsoft.com>
2018-02-23 10:11:50 +0800

    add batch start stop script

---
+++ VM/Batch_start_stop_VM/batch-start-vms.ps1
@@ -0,0 +1,36 @@
+﻿$jobs = @()
+$userinfopath = "D:\user.csv"
+$csvfilepath = "D:\batchstartvm.csv"
+$inputvalues = Import-Csv -Path $csvfilepath 
+$userinfo = Import-Csv -Path $userinfopath
+For ( $x=0; $x -lt $inputvalues.Count;$x++)
+    {
+
+    $rg = $inputvalues[$x].vmrg
+    $vmname = $inputvalues[$x].vmname
+    $params = @($rg, $vmname, $userinfo)
+    $job = Start-Job -ScriptBlock { 
+        param($rg, $vmname,$userinfo)
+        $azureAccountName = $userinfo.username
+        $azurePassword = ConvertTo-SecureString $userinfo.password -AsPlainText -Force
+        $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $azurePassword)
+        Login-AzureRmAccount -Environment 'AzureChinaCloud' -Credential $psCred 
+        Write-Output $rg
+        Write-Output $vmname
+        Start-AzureRmVM -Name $vmname -ResourceGroupName $rg
+     } -ArgumentList $params
+     $jobs = $jobs + $job
+ }
+
+
+If($jobs -ne @())
+  {
+  write-host "Waiting for jobs to complete..." -foregroundcolor yellow -backgroundcolor red
+  wait-job -job $jobs
+  get-job | receive-job
+  }
+Else
+  {
+  write-host "all VMs have been started" -foregroundcolor yellow -backgroundcolor red
+  get-azureRMVm
+  }
